6170d186581ed59fcc5b5efd594aefad9c21ea54
Tredoux555 <tredoux555@gmail.com>
2025-12-19 15:25:45 -0800

    Force Railway rebuild - all files should be present

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+// Admin page - force rebuild
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import AdminDashboard from '@/components/admin/AdminDashboard'
